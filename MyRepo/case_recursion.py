4e782f9657f4cd25c6c31da392fcf21f57734bd3
blakewu <yx.wu.work@gmail.com>
2023-12-01 23:38:53 +0800

    feat: recursion case added

--- MyRepo/case_recursion.py
+++ MyRepo/case_recursion.py
@@ -25,4 +25,43 @@ def inverse_cascade(n):
     
     
 # reverse_cascade(12345)
-inverse_cascade(12345)
+# inverse_cascade(12345)
+
+# hanoi problem
+def move(plate, start, end):
+    print("move plate", plate, "from stick", start, "to stick", end)
+    
+def hanoi(n, start, end):
+    if n == 1:
+        move(n, start, end)
+    else:
+        spare = 6 - start - end
+        hanoi(n-1, start, spare)
+        move(n, start, end)
+        hanoi(n-1, spare, end)
+        
+# hanoi(3, 1, 3)
+
+# Counting Partitions problem
+# break one positive integer into sums of a series of integers smaller 
+# than one designated number in increasing order
+
+def count_partition(n, m):
+    '''default mode return all partitions
+    >>> count_partition(2, 1)
+    >>> 1
+    >>> count_partition(3, 2)
+    >>> 2
+    '''
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif m == 0:
+        return 0
+    else:
+        count = count_partition(n, m-1) + count_partition(n-m, m)
+        return count 
+        
+# test = count_partition(6, 4)
+# print(test)
